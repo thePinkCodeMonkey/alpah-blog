4f0a502474e36433956f7e6dfb56bddb81ed307b
Kitty <thepinkcodemonkey@gmail.com>
2019-03-21 03:55:19 +0000

    Added article list

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,28 @@
 class ArticlesController < ApplicationController
   
+  def index
+    @articles = Article.all
+  end
   def new
     @article = Article.new
   end
   
   def show
     @article = Article.find(params[:id])
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+      flash[:notice] = "Article was successfully edited"
+    else
+      render :edit
+    end
   end
   
   def create
